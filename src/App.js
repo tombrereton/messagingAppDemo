ba15ff6af3ef94bdb9d364e2f8365558e1518d11
Tom Brereton <Tom.Brereton@asos.com>
2018-02-23 11:12:55 +0000

    Big bang of messaging app

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import MessageList from './components/MessageList';
+import MessageBox from './components/MessageBox';
+import Header from './components/Header';
+import firebase from 'firebase';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    var config = {
+      apiKey: process.env.REACT_APP_FIREBASE_KEY,
+      authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+      databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
+      projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+      storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID
+    };
+    firebase.initializeApp(config);
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <Header title="Simple Firebase App" />
+        <div className="columns">
+          <div className="column is-3"></div>
+          <div className="column is-6">
+            <MessageList db={firebase} />
+          </div>
+        </div>
+        <div className="columns" />
+        <div className="column is-3"></div>
+        <div className="column is-6">
+          <MessageBox db={firebase} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
